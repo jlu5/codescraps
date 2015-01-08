fd38f8d3a11fb3e4c0237cb9f4e921e9f1dae00f
James Lu <glolol1@hotmail.com>
2015-01-07 16:25:17 -0800

    fixes to bconv.py -- pain in the ass

--- scraps/bconv.py
+++ scraps/bconv.py
@@ -4,25 +4,28 @@
 import datetime
 import os
 import re
+from sys import argv
 
 yesterday = datetime.datetime.now() - datetime.timedelta(days = 1)
-#filelist = glob.glob('/home/gl/pisglog/ovd_#chat_*.log')
-filelist = ['/home/gl/pisglog/ovd_#chat_%s.log' % \
-    s.strftime('%Y%m%d') for s in (datetime.datetime.now(), yesterday)]
+print(argv)
+if len(argv) >= 2:
+    filelist = argv[1:]
+else:
+    filelist = ['/home/gl/pisglog/ovd_#chat_%s.log' % \
+        s.strftime('%Y%m%d') for s in (datetime.datetime.now(), yesterday)]
 outfile_dir = '/home/gl/pisglog'
 outfile_prefix = ''
 outfile_suffix = '.conv'
 
 ### Converter ###
-convmsg = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?<-\x03\d\d(.*?)\x03>", re.IGNORECASE)
-convme = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?-\x03\d\d(.*?)\x03", re.IGNORECASE)
-convmode = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? \x03\d\d(.*?)\x03 \(.*?\) set mode (.*?) on .*", re.IGNORECASE)
-convmode2 = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? -?(.*?) set mode (.*?) on .*", re.IGNORECASE)
-convjoin = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? \x03\d\d-?(.*?)\x03 \((.*?)\) has joined .*", re.IGNORECASE)
-convpart = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? \x03\d\d-?(.*?)\x03 \((.*?)\) has parted .*", re.IGNORECASE)
-convquit = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? \x03\d\d-?(.*?)\x03 has quit .*", re.IGNORECASE)
-convnick = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? \x03\d\d-?(.*?)\x03 is now known as \x03\d\d(.*?)\x03", re.IGNORECASE)
-convkick = re.compile(r"<(Nebulae.*?|Lily.*?)>.*?\].*? \x03\d\d-?(.*?)\x03 \(.*?\) has been kicked from .*? by (.*?) \((.*?)\)", re.IGNORECASE)
+convmsg = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 <-?(?:\x03|\d)*-?(.*?)\x03?>", re.I)
+convme = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 \* -?(?:\x03|\d)*-?(.*?)\x03?", re.I)
+convmode = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 -?(?:\x03|\d)*-?(.*?)\x03? \(.*?\) set mode (.*?) on .*", re.IGNORECASE)
+convjoin = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 -?(?:\x03|\d)*-?(.*?)\x03? \((.*?)\) has joined .*", re.IGNORECASE)
+convpart = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 -?(?:\x03|\d)*-?(.*?)\x03? \((.*?)\) has parted .*", re.IGNORECASE)
+convquit = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 -?(?:\x03|\d)*-?(.*?)\x03? has quit .*", re.IGNORECASE)
+convnick = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 -?(?:\x03|\d)*-?(.*?)\x03? is now known as \x03\d\d(.*?)\x03", re.IGNORECASE)
+convkick = re.compile(r"<(?:Nebulae.*?|Lily.*?)> \x02\[.+?\]\x02 -?(?:\x03|\d)*-?(.*?)\x03? \(.*?\) has been kicked from .*? by (.*?) \((.*?)\)", re.IGNORECASE)
 
 def _conv(infile):
     inread = open(infile)
@@ -30,15 +33,14 @@ def _conv(infile):
     outfilename = '%s%s%s' % (outfile_prefix, infile, outfile_suffix)
     outfile = open(outfilename, 'w')
     for line in inread:
-        line = convmsg.sub(r"<\2>", line)
-        line = convme.sub(r"* \2", line)
-        line = convmode.sub(r"*** \2 sets mode: \3", line)
-        line = convmode2.sub(r"*** \2 sets mode: \3", line)
-        line = convjoin.sub(r"*** Joins: \2 (\3)", line)
-        line = convpart.sub(r"*** Parts: \2 (\3) ()", line)
-        line = convquit.sub(r"*** Quits: \2 (generic@placeholder.host) ()", line)
-        line = convnick.sub(r"*** \2 is now known as \3", line)
-        line = convkick.sub(r"*** \2 was kicked by \3 (\4)", line)
+        line = convmsg.sub(r"<\1>", line)
+        line = convmode.sub(r"*** \1 sets mode: \2", line)
+        line = convjoin.sub(r"*** Joins: \1 (\2)", line)
+        line = convpart.sub(r"*** Parts: \1 (\2) ()", line)
+        line = convquit.sub(r"*** Quits: \1 (generic@placeholder.host) ()", line)
+        line = convnick.sub(r"*** \1 is now known as \2", line)
+        line = convkick.sub(r"*** \1 was kicked by \2 (\3)", line)
+        line = convme.sub(r"* \1", line)
         outfile.write(line)
     inread.close()
     outfile.close()
