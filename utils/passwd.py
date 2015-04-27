ce5b9211e141605476aa3f2153728bdc5afdd0ef
James Lu <GLolol1@hotmail.com>
2015-04-26 19:53:23 -0700

    passwd.py: python 3 compatibility

--- utils/passwd.py
+++ utils/passwd.py
@@ -1,6 +1,10 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import random
 import string
+import sys
+
+if sys.version_info[0] >= 3:
+    raw_input = input
 
 def passwd(plen=16):
     rg = random.SystemRandom()
@@ -14,13 +18,18 @@ def _fail():
    
 if __name__ == "__main__":
     import sys
-    try: plen = int(sys.argv[1])
-    except IndexError: plen = 16
-    except ValueError: _fail()
-    if plen <= 0: _fail()
-    print passwd(plen)
+    try:
+        plen = int(sys.argv[1])
+    except IndexError:
+        plen = 16
+    except ValueError:
+        _fail()
+    if plen <= 0:
+        _fail()
+    print(passwd(plen))
     try:
         # make it so it doesn't close automatically
         if sys.argv[2] == "--hover":
             raw_input()
-    except: pass
+    except:
+        pass
