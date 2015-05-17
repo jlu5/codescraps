07cf17e08031c956924878e043036226ee4b387c
James Lu <glolol1@hotmail.com>
2015-05-16 21:12:17 -0700

    rplist improvements - get latest snapshot if present, otherwise use the repository
    Loop over all defined repositories instead of a hardcoded list.

--- packagelists/rplist.sh
+++ packagelists/rplist.sh
@@ -1,7 +1,12 @@
-for DIST in "sid" "sid-imports" "experimental" "wheezy-backports"
+for DIST in $(aptly repo list -raw)
 do
     echo Processing lists for $DIST
     distname=`aptly snapshot list -raw | egrep -i ${DIST}-.{4}-.{2}-.{2} | sort -r | cut -d$'\n' -f1`
-    echo "Using packages in snapshot ${distname} for $DIST"
-    aptly snapshot show -with-packages ${distname} | python2 ~/utils/pparse2.py > /srv/aptly/public/${DIST}_list.html
+    if [[ ! -z "$distname" ]]; then
+        echo "Using packages in snapshot ${distname} for $DIST"
+        aptly snapshot show -with-packages ${distname} | python2 ~/utils/pparse2.py > /srv/aptly/public/${DIST}_list.html
+    else
+        echo "Using packages in repo $DIST for $DIST"
+        aptly repo show -with-packages $DIST | python2 ~/utils/pparse2.py > /srv/aptly/public/${DIST}_list.html
+    fi
 done
