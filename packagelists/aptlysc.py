11996d9bbd00cff89d222d9d6bbe79e75dc16030
James Lu <GLolol1@hotmail.com>
2015-08-02 18:47:38 -0700

    Port aptlysc.py to Python 3, and use code safe against command injection

--- packagelists/aptlysc.py
+++ packagelists/aptlysc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import sys
 import subprocess
 import pipes
@@ -9,34 +9,33 @@ import time
 def _error():
     print("error: unknown command, valid commands are 'getdup', 'msnapshot',"
           " 'refreshmirrors', 'purge', 'rsnapshot'")
-    sys.exit(1)    
+    sys.exit(1)
 
 def getdups(reponame):
     if reponame not in repos:
-        print "error: repo '%s' does not exist!"%reponame
+        print("error: repo '%s' does not exist!" % reponame)
         sys.exit(2)
     else:
-        print 'checking duplicates for repo %s...'%reponame
-        showpackages = subprocess.check_output('aptly repo show -with-packages '+pipes.quote(reponame),shell=True).split("\n")
+        print('checking duplicates for repo %s...' % reponame)
+        showpackages = subprocess.check_output(['aptly', 'repo', 'show', '-with-packages', reponame]).decode('utf-8').split("\n")
         uniqPackages = defaultdict(set)
         for p in showpackages:
             p = p.split("_")
-            if len(p) == 3: 
+            if len(p) == 3:
                 packageName, version = p[0].strip(), p[1]
                 uniqPackages[packageName].add(version)
-        for p, v in uniqPackages.iteritems():
-            if len(v) > 1: 
-                print 'Duplicate package: %s (%s)' % (p, ', '.join(v))
+        for p, v in uniqPackages.items():
+            if len(v) > 1:
+                print('Duplicate package: %s (%s)' % (p, ', '.join(v)))
 
 def purge(reponame, packages):
     if reponame not in repos:
-        print "error: repo '%s' does not exist!" % reponame
+        print("error: repo '%s' does not exist!" % reponame)
         sys.exit(2)
     s = ['{0} | $Source ({0})'.format(p) for p in packages]
     s = ' | '.join(s)
     if s:
-        sys.stdout.write(subprocess.check_output('aptly repo remove {} {!r}'.format(
-           reponame, s), shell=True))
+        sys.stdout.write(subprocess.check_output(['aptly', 'repo', 'remove', reponame, s]))
     else:
         print('no matching packages')
         sys.exit(3)
@@ -46,30 +45,31 @@ try:
 except IndexError:
     _error()
 params = sys.argv[2:]
-repos = subprocess.check_output('aptly repo list -raw',shell=True).split('\n')[:-1]
-mirrors = subprocess.check_output('aptly mirror list -raw',shell=True).split('\n')[:-1]
+repos = subprocess.check_output(['aptly', 'repo', 'list', '-raw']).decode('utf-8').split('\n')[:-1]
+mirrors = subprocess.check_output(['aptly', 'mirror', 'list', '-raw']).decode('utf-8').split('\n')[:-1]
+date = time.strftime('%Y-%m-%d')
 
 if command == 'getdup':
     if len(sys.argv) < 3:
-        print 'error: needs repo name!'
+        print('error: needs repo name!')
         sys.exit(2)
     getdups(params[0])
 elif command == 'msnapshot':
     mirrors = params or mirrors
     for mir in mirrors:
-        sys.stdout.write(subprocess.check_output('aptly snapshot create {r}-{d} from mirror {r}'.format(
-                r=mir,d=time.strftime('%Y-%m-%d')), shell=True))
+        sys.stdout.write(subprocess.check_output(['aptly', 'snapshot', 'create', '%s-%s' % (mir, date), 'from', 'mirror', mir]))
+
 elif command == 'rsnapshot':
     repos = params or repos
-    for mir in repos:
-        sys.stdout.write(subprocess.check_output('aptly snapshot create {r}-{d} from repo {r}'.format(
-                r=mir,d=time.strftime('%Y-%m-%d')), shell=True))
+    for repo in repos:
+        sys.stdout.write(subprocess.check_output(['aptly', 'snapshot', 'create', '%s-%s' % (repo, date), 'from', 'repo', repo]))
+
 elif command == 'refreshmirrors':
     for m in mirrors:
-        sys.stdout.write(subprocess.check_output('aptly mirror update %s' % m, shell=True))
+        sys.stdout.write(subprocess.check_output(['aptly', 'mirror', 'update', m]))
 elif command == 'purge':
     if len(sys.argv) < 3:
-        print 'error: needs repo name!'
+        print('error: needs repo name!')
         sys.exit(2)
     purge(params[0], params[1:])
 else:
