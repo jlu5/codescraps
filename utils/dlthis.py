ebfab7d5d1d97323bf1311db4d7a6e342648ee79
James Lu <GLolol@overdrivenetworks.com>
2017-01-08 15:46:42 -0800

    dlthis: implement --author, --playlist template options

--- utils/dlthis.py
+++ utils/dlthis.py
@@ -26,7 +26,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('textfile', nargs='?', help='text file to read links from', default='dlthis.txt')
     parser.add_argument('--encoding', '-e', nargs='?', help='sets the file encoding', default='utf-8')
+    parser.add_argument('--playlist', '-p', action='store_true', help='downloads in playlist mode (output in separate folder, with item number and author in filenames)')
+    parser.add_argument('--author', '-a', action='store_true', help='stores author name in output filename')
     args = parser.parse_args()
+
+    if args.playlist:
+        ydl_opts['outtmpl'] = '%(uploader)s - %(playlist_title)s/%(playlist_index)s %(uploader)s - %(title)s-%(id)s.%(ext)s'
+    elif args.author:
+        ydl_opts['outtmpl'] = '%(uploader)s - %(title)s-%(id)s.%(ext)s'
 
     with open(args.textfile, 'r', encoding=args.encoding) as f:
         files = [link.strip() for link in f.readlines()]
