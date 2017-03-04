49b19656100b3a8ac044966e5b59183663b3f0e8
James Lu <james@overdrivenetworks.com>
2017-03-03 21:53:14 -0800

    reactiontime: spacing fix pls

--- games/reactiontime.py
+++ games/reactiontime.py
@@ -10,7 +10,7 @@ import random
 import threading
 
 if sys.version_info[0] <= 2:  # Python 2 support
-	input = raw_input
+    input = raw_input
 
 print('Instructions: when the string %r shows up, press the Enter key to check your reaction time!' % TARGET_STRING)
 print('Press CTRL-C to exit')
@@ -25,46 +25,46 @@ misses = 0
 results = []
 
 def print_stats():
-	hits = len(results)
-	print()
-	print('          Hits: %s' % hits)
-	print('        Misses: %s' % misses)
-	print('Total attempts: %s' % (hits+misses))
-	if hits:
-		print('     Best time: %f seconds' % min(results))
-		print('    Worst time: %f seconds' % max(results))
-		print('     Avg. time: %f seconds' % (1.0*sum(results)/hits))
+    hits = len(results)
+    print()
+    print('          Hits: %s' % hits)
+    print('        Misses: %s' % misses)
+    print('Total attempts: %s' % (hits+misses))
+    if hits:
+        print('     Best time: %f seconds' % min(results))
+        print('    Worst time: %f seconds' % max(results))
+        print('     Avg. time: %f seconds' % (1.0*sum(results)/hits))
 
 def wait():
-	global popup_time, popup_inactive
-	while True:
-		# Wait a while...
-		time.sleep(random.uniform(MIN_TIME, MAX_TIME))
-		popup_inactive.clear()
+    global popup_time, popup_inactive
+    while True:
+        # Wait a while...
+        time.sleep(random.uniform(MIN_TIME, MAX_TIME))
+        popup_inactive.clear()
 
-		popup_time = time.time()
+        popup_time = time.time()
 
-		print(TARGET_STRING)
+        print(TARGET_STRING)
 
-		popup_inactive.wait()
+        popup_inactive.wait()
 
 t = threading.Thread(target=wait, daemon=True)
 t.start()
 
 while True:
-	try:
-		input()
-	except KeyboardInterrupt:
-		# Allow quitting on Ctrl+C
-		print_stats()
-		sys.exit()
+    try:
+        input()
+    except KeyboardInterrupt:
+        # Allow quitting on Ctrl+C
+        print_stats()
+        sys.exit()
 
-	if popup_inactive.is_set():
-		print('You pressed the button too early! :(')
-		misses += 1
-	else:
-		result = time.time() - popup_time
-		print('Your reaction time: %f seconds' % result)
-		results.append(result)
+    if popup_inactive.is_set():
+        print('You pressed the button too early! :(')
+        misses += 1
+    else:
+        result = time.time() - popup_time
+        print('Your reaction time: %f seconds' % result)
+        results.append(result)
 
-		popup_inactive.set()
+        popup_inactive.set()
