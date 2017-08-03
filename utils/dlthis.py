7c4501321077fa4cea84d672ceaee4c904e86512
James Lu <james@overdrivenetworks.com>
2017-08-03 12:17:39 -0700

    dlthis: add multithreading support via concurrent.futures

--- utils/dlthis.py
+++ utils/dlthis.py
@@ -9,6 +9,12 @@ from __future__ import print_function
 import argparse
 import youtube_dl
 import sys
+import os
+try:
+    import concurrent
+    import concurrent.futures
+except ImportError:
+    concurrent = None
 
 if sys.version_info[0] < 3:
     # Use io.open() on Python 2 for encoding support.
@@ -28,6 +34,7 @@ if __name__ == '__main__':
     parser.add_argument('--encoding', '-e', nargs='?', help='sets the file encoding', default='utf-8')
     parser.add_argument('--playlist', '-p', action='store_true', help='downloads in playlist mode (output in separate folder, with item number and author in filenames)')
     parser.add_argument('--author', '-a', action='store_true', help='stores author name in output filename')
+    parser.add_argument("-j", "--jobs", help="amount of threads to use (requires Python 3, defaults to amount of CPU cores)", type=int, default=os.cpu_count() or 1)
     args = parser.parse_args()
 
     if args.playlist:
@@ -40,6 +47,21 @@ if __name__ == '__main__':
 
     with open(args.textfile, 'r', encoding=args.encoding) as f:
         files = [link.strip() for link in f.readlines()]
+        # Filter out empty lines and those starting with # (comments)
+        files = [link for link in files if link and not link.startswith('#')]
+
         print("Downloading: %s" % files, file=sys.stderr)
         with youtube_dl.YoutubeDL(YTDL_OPTS) as ydl:
-            ydl.download(files)
+            if concurrent is not None and args.jobs > 1:
+                import threading
+                def download_wrapper(link):
+                    print('Downloading file %s in thread %s' % (link, threading.current_thread().name), file=sys.stderr)
+                    return ydl.download([link])
+
+                with concurrent.futures.ThreadPoolExecutor(max_workers=args.jobs) as pool:
+                    for future in pool.map(download_wrapper, files):
+                        # We need to iterate over the map() object for it to actually run.
+                        pass
+            else:
+                print('Disabling multithreading mode (needs Python 3 and job count > 1')
+                ydl.download(files)
