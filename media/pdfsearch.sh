f910d578b283972dd2d25e5ea32b36af7627d07a
James Lu <james@overdrivenetworks.com>
2019-06-04 22:32:25 -0700

    pdfsearch: support other kinds of docs via pandoc

--- media/pdfsearch.sh
+++ media/pdfsearch.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-# Searches a string of text (first argument) through a list of PDF files.
+# Searches a string of text (first argument) through a list of documents.
 
 QUERY="$1"
 
 shift
 
-for pdf in "$@"; do
-	echo "SEARCHING: $pdf"
-	pdftotext "$pdf" - | grep -i "$QUERY"
+bold="$(tput bold)"
+normal="$(tput sgr0)"
+
+search () {
+	while read data; do
+		grep --color -i "$QUERY" <<< "$data"
+	done
+}
+
+for file in "$@"; do
 	echo ""
+	echo "${bold}SEARCHING: ${file}${normal}"
+
+	# Use pdftotext for PDFs, or pandoc for everything else
+	if [[ "$(file --mime-type -b "$file")" == "application/pdf" ]]; then
+		pdftotext "$file" - | search
+	else
+		pandoc "$file" -t plain --wrap=none -o - | search
+	fi
 done
