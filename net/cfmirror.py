ca6d0243733b459fae3e40fd8fd3b1b1cfd1549b
James Lu <james@overdrivenetworks.com>
2019-08-19 20:08:03 -0700

    cfmirror: fix scraper usage

--- net/cfmirror.py
+++ net/cfmirror.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
 Downloads a static web page protected by Cloudflare.
 This can be used to unblock, for example, RSS feeds behind its anti-bot page.
@@ -10,13 +11,15 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument('source', help='source URL')
 parser.add_argument('target', help='target file')
 parser.add_argument('--delay', type=int, help="time to wait during Cloudflare's challenge page", default=5)
+parser.add_argument('--cookie-file', type=str, help="location (relative to cwd) to save Cloudflare cookie", default='cloudflare-cookie')
 
 args = parser.parse_args()
 
-scraper = cfscrape.create_scraper(delay=args.delay)
-site = scraper.get(args.source)
-print('Got Cloudflare cookies: %s' % cfscrape.get_tokens())
+sess = cfscrape.create_scraper(delay=args.delay)
 
+print('Waiting for %s ...' % args.source)
+
+site = sess.get(args.source)
 with open(args.target, 'wb') as outf:
     count = outf.write(site.content)
     print('Wrote %d bytes to %s' % (count, args.target))
